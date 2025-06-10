1b5a0d33b601fa2d283206fb9db42c08b8c525c0
ilKusco <91097118+ilKusco@users.noreply.github.com>
2025-06-10 11:52:06 +0200

    Update MonadLiveRPC.jsx

--- src/components/MonadLiveRPC.jsx
+++ src/components/MonadLiveRPC.jsx
@@ -87,7 +87,7 @@ export default function MonadLiveRPC() {
 
   useEffect(() => {
     fetchData();
-    const interval = setInterval(fetchData, 10000);
+    const interval = setInterval(fetchData, 1000);
     return () => clearInterval(interval);
   }, []);
 
